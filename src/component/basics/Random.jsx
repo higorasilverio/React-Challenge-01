f28ffde2d26936dfc35b2203b003ccd79fa0e90e
higorasilverio <higorasilverio@outlook.com>
2021-04-01 20:18:00 -0300

    Using destructuring assignment to enhance code readability

--- src/component/basics/Random.jsx
+++ src/component/basics/Random.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default (props) => {
-    const randomNumber = Math.random() * (props.max - props.min) + props.min
+    const { min, max } = props
+    const randomNumber = Math.random() * (max - min) + min
     return (
         <div style={{ marginLeft: 50 }}>
             <h2>Random Number Challenge:</h2>
-            <p>Minimum value: <strong>{ props.min }</strong></p>
-            <p>Maximum value: <strong>{ props.max }</strong></p>
+            <p>Minimum value: <strong>{ min }</strong></p>
+            <p>Maximum value: <strong>{ max }</strong></p>
             <p>Value generated: <strong>{ Math.round(randomNumber) }</strong></p>
             <h4>Refresh this page to generate another random number!</h4>
         </div>
